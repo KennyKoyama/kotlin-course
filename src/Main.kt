caaa45fee85f17f65eaf068fc429e4a87e206c93
KennyKoyama <kennysk1982@gmail.com>
2024-02-22 21:45:55 -0300

    Exercise 3 and 4 Done

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,29 @@
+import java.text.DecimalFormat
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-  //Exercise 1
-  val currentSalary = 1000.0
-  val newSalary: Double = increaseSalary(currentSalary)
-  println("The salary has increased from $currentSalary to $newSalary")
+  // Exercise 1
+  increaseSalary(1000.0)
 
-  //Exercise 2
-  val stepHeight = 0.20
-  val targetHeight = 10.0
-  val totalSteps = calculateSteps(stepHeight, targetHeight)
-  println("For the person to climb $targetHeight meters knowing that the height" +
-      "of the step is $stepHeight meters, he needs to climb $totalSteps steps")
+  // Exercise 2
+  calculateSteps(0.20, 10.0)
+
+  // Exercise 3
+  biggestNumber(5, 10)
+  biggestNumber(10, 5)
+
+  // Exercise 4
+  evaluateGrades(1.1, 2.2, 3.3)
+  evaluateGrades(5.2, 6.3, 7.4)
+  evaluateGrades(6.5, 7.6, 8.7)
 }
 
 //Exercício 1
 //Escreva um programa usando Kotlin que receba o salário de uma pessoa colaboradora,
 // calcule e mostre o novo salário, sabendo-se que este sofreu um aumento de 25%.
-fun increaseSalary(salary: Double): Double {
-  return salary * 1.25
+fun increaseSalary(salary: Double) {
+  println("The salary has increased from $salary to ${salary * 1.25}.")
 }
 
 //Exercício 2
@@ -27,13 +32,17 @@ fun increaseSalary(salary: Double): Double {
 // deseja alcançar usando a escada, calcule e mostre quantos degraus ela deverá subir
 // para atingir seu objetivo, sem se preocupar com a altura da pessoa. Todas as medidas
 // fornecidas devem estar em metros.
-fun calculateSteps(stepHeight: Double, targetHeight: Double): Double {
-  return targetHeight / stepHeight
+fun calculateSteps(stepHeight: Double, targetHeight: Double) {
+  println("For the person to climb $targetHeight meters knowing that the height" +
+      "of the step is $stepHeight meters, he needs to climb ${targetHeight / stepHeight}" +
+      "steps.")
 }
 
 //Exercício 3
 //Escrevas um programa em Kotlin que receba dois números e mostre o maior.
-
+fun biggestNumber(num1: Int, num2: Int) {
+  println("The biggest number between $num1 and $num2 is ${if (num1 > num2) num1 else num2}.")
+}
 
 //Exercício 4
 //Escrevas um programa em Kotlin que receba três notas de uma pessoa estudante e mostre
@@ -41,4 +50,15 @@ fun calculateSteps(stepHeight: Double, targetHeight: Double): Double {
 //Média aritmética igual ou maior que 0.0 e menor do que 3.0, exibir a mensagem REPROVADO
 //Média aritmética igual ou maior que 3.0 e menor do que 7.0, exibir a mensagem EXAME
 //Média aritmética igual ou maior que 7.0 e menor do que 10.0, exibir a mensagem APROVADO
+fun evaluateGrades(grade1: Double, grade2: Double, grade3: Double) {
+  val average = (grade1 + grade2 + grade3) / 3
+  val df = DecimalFormat("#.##")
 
+  if (average < 3) {
+    println("The average grade is ${df.format(average)}. REPROVADO.")
+  } else if (average >= 3 && average < 7) {
+    println("The average grade is ${df.format(average)}. EXAME.")
+  } else {
+    println("The average grade is ${df.format(average)}. APROVADO.")
+  }
+}
